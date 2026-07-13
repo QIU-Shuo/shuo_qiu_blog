25178eb09f51a7657cc5faa56c5fd810238b05bc
Shuo Qiu <xuexuexun@gmail.com>
2026-07-13 00:43:58 -0700

    Canonicalize site on www.shuoq.dev (#4)

--- blog/next.config.ts
+++ blog/next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "shuoq.dev",
+          },
+        ],
+        destination: "https://www.shuoq.dev/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
